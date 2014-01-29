9391aac3ea73900149a6e7b7152aebdb864de0de
Tommy Ettinger <tommy.ettinger@gmail.com>
2014-01-28 22:40:51 -0800

    Added bag, called partition by A+ APL

--- src/inseqt/core.clj
+++ src/inseqt/core.clj
@@ -24,8 +24,13 @@
     (count coll)
     0))
 (defn sculpt
+  "Like reshape, but it will cycle the original matrix if it isn't long enough
+   to fill the requested shape (rather than throwing an error).  The matrix m
+   must have at least one scalar element."
   [m sh]
-  (reshape (take (apply * sh) (cycle (unnest [m]))) sh))
+  (if-let [shp (seq (remove zero? sh))]
+    (reshape (take (apply * shp) (cycle (unnest [m]))) (vec shp))
+    (first (reshape m [1]))))
 (def each emap)
 (def depth dimensionality)
 (def enclose vector)
@@ -38,6 +43,23 @@
 (def iota interval)
 (def sum esum)
 
+(defn bag
+  "Returns a nested vector, acting like partition-all, but can take a vector or
+   a width to partition.  The vector must contain at least one non-negative
+   number of columns to take from the matrix and store in the result."
+  ([n coll]
+     (cond
+       (number? n) (bag n n coll)
+       (sequential? n) (loop [bagger n bags [] unbagged coll]
+                         (if (seq bagger)
+                           (recur (rest bagger) (conj bags (vec (take (first bagger) unbagged))) (drop (first bagger) unbagged))
+                           bags))
+       :else coll))
+  ([n step coll]
+     (vec
+      (when-let [s (seq coll)]
+        (let [seg (vec (take n s))]
+          (cons seg (bag n step (nthrest s step))))))))
 
 (comment
 "
